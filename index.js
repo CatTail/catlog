33639cc99810e716fba9723bd4c2d69049948a99
cattail <cattail2012@gmail.com>
2013-05-12 22:45:47 +0800

    add category feature

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var fs = require('fs')
   , util = require('util')
   , exec = require('child_process').exec
   , path = require('path')
+  , async = require('async')
   , parser = require('./lib/parser')
   , render = require('./lib/render')
   , directory = require('./lib/directory')
@@ -9,17 +10,14 @@ var fs = require('fs')
 //  , ncp = require('ncp')
   , _ = require('underscore');
 
-var gen_post = function gen_post (env) {
+var gen_post = function gen_post (env, callback) {
   parser.parse_post(env);
   render.render_post(env, function () {
     !fs.existsSync(path.dirname(env.dest)) &&
       directory.mkdir_parent(path.dirname(env.dest));
     fs.writeFileSync(env.dest, env.post, 'utf8');
+    callback && callback();
   });
-};
-
-var gen_list = function gen_list (env) {
-  
 };
 
 var updater = function updater (env) {
@@ -30,29 +28,45 @@ var updater = function updater (env) {
 var main = function main () {
   var default_settings = require('./settings')
     , settings = _.defaults({}/* global_settings */, default_settings)
+    , categories
     , envs = [];
 
   directory.traverse(settings.source, function (src) {
     var env = _.clone(settings);
     if (fs.statSync(src).isFile() && path.extname(src) === '.md') {
-      envs.push(env);
       env.src = src;
-      gen_post(env);
-      fs.watchFile(src, {persistent: true, interval: 1000}, function () {
+      envs.push(env);
+      parser.parse_post(env);
+    }
+  });
+  categories = parser.parse_categories(envs);
+  // render post
+  async.each(envs, function (env, callback) {
+    render.render_post(env, function () {
+      !fs.existsSync(path.dirname(env.dest)) &&
+        directory.mkdir_parent(path.dirname(env.dest));
+      fs.writeFileSync(env.dest, env.post, 'utf8');
+      fs.watchFile(env.src, {persistent: true, interval: 1000}, function () {
         updater(env);
       });
-    }
+      callback();
+    });
+  }, function () {
+    console.log('complete');
   });
   // render index
   var env = _.clone(settings);
   env.articles = envs;
+  env.categories = categories;
   render.render_list(env);
   fs.writeFileSync(env.destination+'/index.html', env.post, 'utf8');
   // copy themes assets
   // FIXME
-  exec(util.format('cp -r ./themes/%s %s;mv %s/%s %s/theme', 
-                   settings.theme, settings.destination, settings.destination, 
-                   settings.theme, settings.destination));
+  exec(util.format('rm -r ./%s/themes/*', settings.destination), function () {
+    exec(util.format('cp -rf ./themes/%s %s;mv %s/%s %s/theme', 
+                     settings.theme, settings.destination, settings.destination, 
+                     settings.theme, settings.destination));
+  });
 //  ncp('themes/'+settings.theme, settings.destination, function () {});
   server.run({root: settings.destination, port: settings.port});
 };
